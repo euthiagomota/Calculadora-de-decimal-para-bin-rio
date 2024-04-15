9c1f80aa92af28be0ab2a2a0b961e747edc51431
Thiago Mota <euthiagojose2004@gmail.com>
2024-04-15 18:21:28 -0300

    fiz alterações na validação da calculadora.

--- src/main.js
+++ src/main.js
@@ -4,22 +4,28 @@ function calcular() {
     // Seleciona o input e o botão
     const inputElement = document.getElementById("number");
     const buttonElement = document.getElementById("botao");
-    
+
     // Adiciona um event listener ao botão para reagir ao clique
-        buttonElement.addEventListener('click', function() {
-    
-    // Obtém o valor do input quando o botão é clicado
+    buttonElement.addEventListener('click', function () {
+
+        // Obtém o valor do input quando o botão é clicado
         let numero = inputElement.value;
-    
-    
         let binario = ""
-    
-        for(; numero > 0; numero = Math.floor(numero/2) ) {
-            let resto = numero % 2
-            binario = resto + binario
-   document.getElementById("resultado").textContent = "O número binário correspondente é: " + binario;
+
+        if (numero <= 9 || numero >= 100) {
+            document.getElementById('resultado').textContent = "Erro! Digite um número decimal entre 10 e 99. "
+        }
+        else if(isNaN(numero)) {
+            document.getElementById('resultado').textContent = "Erro! Digite um número. "
         }
         
+        else {
+            for (; numero > 0; numero = Math.floor(numero / 2)) {
+                let resto = numero % 2
+                binario = resto + binario
+                document.getElementById('resultado').textContent = "O número binário correspondente é: " + binario;
+            }
+        }
     });
 }
 
